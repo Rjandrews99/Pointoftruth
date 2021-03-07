fa4693966193d988e5a2dae2d8ed5bb5e7b79c18
Rjandrews99 <reece.andrews@icloud.com>
2021-03-07 20:30:04 +0000

    Initial commit

---
+++ JS/mobileNav.js
@@ -0,0 +1,9 @@
+function mobileNav() {
+    var x = document.getElementById("mobileNav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
